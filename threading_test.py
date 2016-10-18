bf1e8ff7a26bad5e626e9a1d54c4bf5ac10f4776
Adi Foulger <technobabelfish@gmail.com>
2016-10-17 20:22:10 -0700

    adventure!

--- threading_test.py
+++ threading_test.py
@@ -1,22 +1,34 @@
 from threading import Thread
+from uuid import uuid4
+from random import selection, getrandbits
+
 from redis import StrictRedis, ConnectionPool
 
+
 NUMBER_OF_CONNECTIONS = 10
+NUMBER_OF_THREADS = 50
+NUMBER_OF_KEYS = 100
+SIZE_OF_VALUE = 8*10*1024
+
+GUIDS = []
 
 R = lambda: StrictRedis(connection_pool=ConnectionPool(**{'host':'redis-13034.lace.demo-rlec.redislabs.com',
-
-                                        'port':13034, 'max_connections':NUMBER_OF_CONNECTIONS}))
+                                                          'port':13034,
+                                                          'max_connections':NUMBER_OF_CONNECTIONS}))
 
 def hello_dummy():
+    r = R()
     while True:
-        r = R()
-        r.set('foo', 'bar')
-        r.get('foo')
+        r.set(choice(GUIDS), getrandbits(SIZE_OF_VALUE))
+        #r.get('foo')
 
 
 if __name__ == '__main__':
+    for i in range(NUMBER_OF_KEYS):
+        GUIDS.append(uuid4)
+
     threads = []
-    for i in range(50):
+    for i in range(NUMBER_OF_THREADS):
         t = Thread(target=hello_dummy)
         t.start()
         threads.append(t)
