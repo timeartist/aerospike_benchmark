f8871845eb334ce556d259bcde9ce181dd12121f
Adi Foulger <technobabelfish@gmail.com>
2016-10-17 19:51:48 -0700

    Adventure!

--- threading_test.py
+++ threading_test.py
@@ -1,4 +1,8 @@
 from threading import Thread
+from redis import StrictRedis, ConnectionPool
+
+R = StrictRedis(ConnectionPool({'host':'redis-13034.lace.demo-rlec.redislabs.com',
+                 'port':13034}))
 
 def hello_dummy():
     print 'hi!'
@@ -8,5 +12,3 @@ if __name__ == '__main__':
     t = Thread(target=hello_dummy)
     t.start()
     t.join()
-
-    
