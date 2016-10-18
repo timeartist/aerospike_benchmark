4ec0907ab6c681d83370eb5a5a49800981cc2004
Ubuntu <ubuntu@ip-172-31-53-120.ec2.internal>
2016-10-18 03:13:42 +0000

    serverside tweaks

--- threading_test.py
+++ threading_test.py
@@ -3,16 +3,23 @@ from redis import StrictRedis, ConnectionPool
 
 NUMBER_OF_CONNECTIONS = 10
 
-R = lambda: StrictRedis(ConnectionPool({'host':'redis-13034.lace.demo-rlec.redislabs.com',
-                                        'port':13034, max_connections=NUMBER_OF_CONNECTIONS}))
+R = lambda: StrictRedis(connection_pool=ConnectionPool(**{'host':'redis-13034.lace.demo-rlec.redislabs.com',
+
+                                        'port':13034, 'max_connections':NUMBER_OF_CONNECTIONS}))
 
 def hello_dummy():
-    r = R()
-    print r.set('foo', 'bar')
-    print r.get('foo')
+    while True:
+        r = R()
+        r.set('foo', 'bar')
+        r.get('foo')
 
 
 if __name__ == '__main__':
-    t = Thread(target=hello_dummy)
-    t.start()
-    t.join()
+    threads = []
+    for i in range(50):
+        t = Thread(target=hello_dummy)
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
